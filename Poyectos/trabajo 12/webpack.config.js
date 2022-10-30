1534a0a762e97756d31711e8e8a239038154d94b
willianstars <willian.15102017@gmail.com>
2022-10-30 15:02:02 -0300

    Update

---
+++ Poyectos/trabajo 12/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/app.js',
+    output: {
+    filename: 'bundle.js',
+    	path: path.join(__dirname, 'dist'),
+        		},
+        		mode: "production",
+        		module: {
+            		rules: [
+                		{
+                    		test: /\.m?js$/,
+                    		exclude: /node_modules/,
+                    		use: {
+                        		loader: "babel-loader",
+                        		options: {
+                            		presets: ['@babel/preset-env']
+                        		}
+                    		}
+                		},
+                        {
+                            test: /\.css$/,
+                            use: [
+                             'style-loader',
+                             'css-loader'
+                            ]
+                         }
+            		]
+        		}
+    		}; 
